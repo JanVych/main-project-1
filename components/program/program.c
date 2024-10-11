f76e6d6e17d7c5ad090b94ee6c82ba4ed313ff54
JanVych <132760222+JanVych@users.noreply.github.com>
2024-10-12 00:09:18 +0200

    etatherm 1.0

--- components/program/program.c
+++ components/program/program.c
@@ -2,6 +2,7 @@
 #include "program.h"
 #include "esp_log.h"
 #include "freertos/FreeRTOS.h"
+#include "driver/uart.h"
 
 #include "server_comm.h"
 #include "etatherm.h"
@@ -14,7 +15,6 @@ static void defaultCallback(char* str){
 
 void log_actions()
 {
-    //ESP_LOGI(TAG, "hello");
     ESP_LOGI(TAG, "free memory: %i", (int)esp_get_free_heap_size());
 }
 
@@ -23,23 +23,27 @@ void program()
     // eta_err_t result;
     // uint8_t value = 0;
     // commAddAction("test-program", defaultCallback);
-    // result = etaInit();
+    // result = eta_Init(UART_NUM_2, 17, 16);
     // ESP_LOGI(TAG, "uart init: %d", result);
-    // uint8_t room = 0;
-    // uint8_t setval = 13;
+    // uint8_t room = 2;
+    // uint8_t setval = 14;
     while(true)
     {
         log_actions();
-        // result = etaGetRealTemp(room, &value);
+        // result = eta_GetRealTemp(1, room, &value);
         // ESP_LOGI(TAG, "real temp result: %d, room: %u, value: %u", result, room, value);
-        // result = etaGetDesiredTemp(room, &value);
+        // result = eta_GetDesiredTemp(1, room, &value);
         // ESP_LOGI(TAG, "desired temp result: %d, room: %u, value: %u", result, room, value);
-        // result = etaGetOzTemp(room, &value);
+        // result = eta_GetOzTemp(1, room, &value);
         // ESP_LOGI(TAG, "oz temp result: %d, room: %u, value: %u", result, room, value);
 
-        // result = etaSetOzTemp(0, setval);
+        // result = eta_SetOzTemp(1,room, setval);
         // ESP_LOGI(TAG, "set oz temp result: %d, room: %u, value: %u", result, room, setval);
 
-        // vTaskDelay(20000 / portTICK_PERIOD_MS);
+        // result = eta_GetTempShift(1, 0, &value);
+        // ESP_LOGI(TAG, "type value: %d, room: %u, value: %u", result, room, value);
+
+
+        vTaskDelay(20000 / portTICK_PERIOD_MS);
     }
 }
